bc9570be71bee5966705fb8fa901417d347266b3
Austin Gray <Austin.m.gray210@gmail.com>
2020-03-19 10:52:36 -0500

    finished HelloWorld exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+// TODO: Create a program that outputs "Hello, World!" to the console.
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
+
+// TODO: Run the application using the "Run" menubar item in IntelliJ (Note there are many ways to run a Java file). IntelliJ will first compile your HelloWorld.java file, then execute the compiled code.
+
+// TODO: Look at your project structure. You should notice a new directory named out. This is where IntelliJ puts the compiled Java files, to keep them separate from the source code.
+
+// TODO: Try renaming the main method. What happens when you try to run the program?
+// It disables the class name and doesn't allow the program to run
+
+// TODO: System.out.println is used to output information to the console with a newline. System.out.print can be used to output information without a newline. Refactor your code to use two System.out.print statements without changing the output.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+}
