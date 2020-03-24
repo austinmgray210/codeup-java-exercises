25e612574c9cec33a93b96eadf9ccab393b1c65b
Austin Gray <Austin.m.gray210@gmail.com>
2020-03-23 20:57:11 -0500

    unfinished

---
+++ src/HighLow.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+
+        int randomNum = (int) (Math.floor(Math.random() * 100) + 1);
+        playHighLow(randomNum);
+
+    }
+
+    // TODO: Game picks a random number between 1 and 100.
+    // TODO: Prompts user to guess the number.
+
+    public static void playHighLow(int randomNum) {
+        System.out.println("Do you want to play High/Low?");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next();
+        boolean userWantsToPlay = userInput.equals("y");
+
+            if (userWantsToPlay) {
+                int randomNum = (int) (Math.floor(Math.random() * 100) + 1);
+                getUserNumber(randomNum);
+            }
+    }
+
+    public static void getRandomNumber() {
+    }
+
+
+    public static void getUserNumber(int randomNum) {
+
+        System.out.println("Choose a number between 1 and 100.");
+        Scanner userInput = new Scanner(System.in);
+        int userNum = userInput.nextInt();
+        verifyUserNumber(userNum);
+    }
+
+    public static void verifyUserNumber(int userNum) {
+        if ((userNum > 100) || (userNum < 1)) {
+            getUserNumber();
+        } else {
+            highLowGameLogic(userNum);
+        }
+    }
+
+    public static void highLowGameLogic(int userNum) {
+        int counter = 50;
+
+        if (userNum == randomNum) {
+            System.out.println("GOOD GUESS!");
+        } else if (userNum > randomNum) {
+            System.out.println("LOWER");
+            counter--;
+            System.out.printf("You have %d lives left.\n", counter);
+            getUserNumber();
+        } else if (userNum < randomNum) {
+            System.out.println("HIGHER");
+            counter--;
+            System.out.printf("You have %d lives left.\n", counter);
+            getUserNumber();
+        }
+    }
+
+
+
+}
+
+
+
+
